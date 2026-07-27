8e5340f3f31a29963ed4f61ebfdf92a52db6278d
FuncProgLinux <softwarelibre@urutau-ltd.org>
2026-07-27 15:59:14 -0600

    Change font

--- _config.ts
+++ _config.ts
@@ -106,7 +106,7 @@ site.use(redirects());
 
 site.use(googleFonts({
     fonts:
-        "https://fonts.googleapis.com/css2?family=Neuton:ital,wght@0,200;0,300;0,400;0,700;0,800;1,400&display=swap",
+        "https://fonts.googleapis.com/css2?family=Lora:ital,wght@0,400..700;1,400..700&display=swap",
     cssFile: "/urutau.css",
 }));
 
