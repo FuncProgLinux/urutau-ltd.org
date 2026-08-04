59529e6afc769596147826a4aa774c79390ffb2f
FuncProgLinux <softwarelibre@urutau-ltd.org>
2026-08-04 13:17:23 -0600

    Add license identifiers to LibreJS scripts

--- src/public/js/contact_reveal.js
+++ src/public/js/contact_reveal.js
@@ -1,3 +1,4 @@
+// @license magnet:?xt=urn:btih:0b31508aeb0634b347b8270c7bee4d411b5d4109&dn=agpl-3.0.txt AGPL-3.0-or-later
 // SPDX-License-Identifier: AGPL-3.0-or-later
 // Copyright © 2026 Urutau-Ltd <softwarelibre@urutau-ltd.org>
 
@@ -146,3 +147,4 @@
         bindAllChallenges();
     }
 })();
+// @license-end
