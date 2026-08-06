a3105c53e72d304fe8856a840136b41754576401
FuncProgLinux <softwarelibre@urutau-ltd.org>
2026-08-06 17:58:27 -0600

    remove unused server code

--- serve.ts
+++ serve.ts
@@ -1,9 +1,7 @@
 import Server from "lume/core/server.ts";
 import notFound from "lume/middlewares/not_found.ts";
 
-import ApiRouter from "$urutau/routes";
-
-const port: number = Number(Deno.env.get("URUTAU_PORT") || 8000);
+const port: number = 8000;
 const root = `${Deno.cwd()}/output`;
 const server: Server = new Server({ port, root });
 
@@ -11,6 +9,5 @@ server.use(notFound({
     root,
     page404: "/404.html",
 }));
-server.use(ApiRouter.middleware());
 
 server.start();
